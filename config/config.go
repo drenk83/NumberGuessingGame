b4e03db0145fbe2e2b2c3f70b6046cd1a630d332
Egor Chuprov <ytdrenk@gmail.com>
2025-11-28 23:26:35 +0300

    new config

--- config/config.go
+++ config/config.go
@@ -1,115 +1,149 @@
 package config
 
-import (
-	"errors"
+type GameMode int
+type Language int
+
+// Consts
+// ---------------------------------
+const (
+	ClassicMode GameMode = iota
+	TimedMode
 )
 
 const (
-	ModeDefault = "default"
-	ModeBot     = "botmode"
-
-	DefAttempt = 7
-
-	Eng = "eng"
-	Rus = "ru"
+	LangEng Language = iota
+	LangRus Language
 )
 
+const (
+	DefaultMode        GameMode = ClassicMode
+	DefaultMaxAttempts int      = 7
+	DefaultLanguage    Language = LangEng
+	DefaultMaxNumber   int      = 100
+)
+
+// Struct
+// ---------------------------------
 type GameConfig struct {
-	gameMode    string
+	mode        GameMode
 	maxAttempts int
-	language    string
-	min         int
-	max         int
+	language    Language
+	maxNumber   int
 }
 
-func MakeCfg(m *string, a *int, l *string) GameConfig {
-	return GameConfig{
-		gameMode:    *m,
-		maxAttempts: *a,
-		language:    *l,
-		min:         1,
-		max:         100,
+// Constructors
+// ---------------------------------
+func NewGameConfig() *GameConfig {
+	return &GameConfig{
+		mode:        DefaultMode,
+		maxAttempts: DefaultMaxAttempts,
+		language:    DefaultLanguage,
+		maxNumber:   DefaultMaxNumber,
 	}
 }
 
 // Getters
 // ---------------------------------
-func (g *GameConfig) Mode() string {
-	return g.gameMode
-}
+// func (g *GameConfig) Mode() string {
+// 	return g.gameMode
+// }
 
-func (g *GameConfig) MaxAtt() int {
-	return g.maxAttempts
-}
+// func (g *GameConfig) MaxAtt() int {
+// 	return g.maxAttempts
+// }
 
-func (g *GameConfig) Lang() string {
-	return g.language
-}
+// func (g *GameConfig) Lang() string {
+// 	return g.language
+// }
 
-func (g *GameConfig) Min() int {
-	return g.min
-}
+// func (g *GameConfig) Min() int {
+// 	return g.min
+// }
 
-func (g *GameConfig) Max() int {
-	return g.max
-}
+// func (g *GameConfig) Max() int {
+// 	return g.max
+// }
 
 // Setters
 // --------------------------------
-func (g *GameConfig) SetMode(m string) error {
-	switch m {
-	case ModeDefault:
-		g.gameMode = ModeDefault
-		return nil
+// func (g *GameConfig) SetMode(m string) error {
+// 	switch m {
+// 	case ModeDefault:
+// 		g.gameMode = ModeDefault
+// 		return nil
 
-	case ModeBot:
-		g.gameMode = ModeBot
-		return nil
+// 	case ModeBot:
+// 		g.gameMode = ModeBot
+// 		return nil
 
+// 	default:
+// 		return errors.New("[ERROR] in changing the mode")
+// 	}
+// }
+
+// func (g *GameConfig) SwapMode() {
+// 	if g.gameMode == ModeDefault {
+// 		g.gameMode = ModeBot
+// 	} else {
+// 		g.gameMode = ModeDefault
+// 	}
+// }
+
+// func (g *GameConfig) SetLang(l string) error {
+// 	switch l {
+// 	case Eng:
+// 		g.language = Eng
+// 		return nil
+
+// 	case Rus:
+// 		g.language = Rus
+// 		return nil
+
+// 	default:
+// 		return errors.New("[ERROR] in changing the language")
+// 	}
+// }
+
+// func (g *GameConfig) SetAttempts(a int) error {
+// 	if a <= 0 {
+// 		return errors.New("[ERROR] Zero or negative number of attempts")
+// 	}
+// 	g.maxAttempts = a
+// 	return nil
+// }
+
+// func (g *GameConfig) SetMinMax(mn, mx int) error {
+// 	if mn > mx {
+// 		return errors.New("[ERROR] Min is more than max")
+// 	}
+// 	if mn == mx {
+// 		return errors.New("[ERROR] Same values")
+// 	}
+// 	g.min = mn
+// 	g.max = mx
+// 	return nil
+// }
+
+// Strings
+// --------------------------------
+func (g GameMode) String() string {
+	switch g {
+	case ClassicMode:
+		return "Classic"
+	case TimedMode:
+		return "Timed"
 	default:
-		return errors.New("[ERROR] in changing the mode")
+		return "Unknown"
 	}
 }
 
-func (g *GameConfig) SwapMode() {
-	if g.gameMode == ModeDefault {
-		g.gameMode = ModeBot
-	} else {
-		g.gameMode = ModeDefault
-	}
-}
-
-func (g *GameConfig) SetLang(l string) error {
+func (l Language) String() string {
 	switch l {
-	case Eng:
-		g.language = Eng
-		return nil
-
-	case Rus:
-		g.language = Rus
-		return nil
-
+	case LangEng:
+		return "English"
+	case LangRus:
+		return "Russian"
 	default:
-		return errors.New("[ERROR] in changing the language")
+		return "Unknown"
 	}
-}
-
-func (g *GameConfig) SetAttempts(a int) error {
-	if a <= 0 {
-		return errors.New("[ERROR] Zero or negative number of attempts")
-	}
-	g.maxAttempts = a
-	return nil
-}
-
-func (g *GameConfig) SetMinMax(mn, mx int) error {
-	if mn > mx {
-		return errors.New("[ERROR] Min is more than max")
-	}
-	if mn == mx {
-		return errors.New("[ERROR] Same values")
-	}
-	g.min = mn
-	g.max = mx
-	return nil
 }
